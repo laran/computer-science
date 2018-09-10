4c5ae4a90620ae69104c2c1ac31af4a6c48711dc
Laran Evans <laran@laranevans.com>
2018-09-10 12:05:04 -0700

    Implementation of all permutations of a String.

---
+++ src/test/java/com/laranevans/algorithms/strings/PermuteTest.java
@@ -0,0 +1,19 @@
+/* (C) Copyright 2017-2018 Laran Evans */
+package com.laranevans.algorithms.strings;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class PermuteTest {
+
+	@Test
+	public void permuteStrings() {
+		Assert.assertThat(new Permute().permute("abc"), Matchers.equalTo(
+			Arrays.asList("abc", "acb", "bac", "bca", "cba", "cab")
+		));
+	}
+
+}
