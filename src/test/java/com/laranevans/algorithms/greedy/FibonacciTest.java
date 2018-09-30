3a6ee1b7777d94c0d51c57ef0340eec590c51f4c
Laran Evans <laran@laranevans.com>
2018-09-30 06:06:33 -0700

    Implemented Fibonacci

---
+++ src/test/java/com/laranevans/algorithms/greedy/FibonacciTest.java
@@ -0,0 +1,20 @@
+/* (C) Copyright 2017-2018 Laran Evans */
+package com.laranevans.algorithms.greedy;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FibonacciTest {
+
+	@Test
+	public void testFib() {
+		Assert.assertThat(Fibonacci.fib(0), Matchers.equalTo(0L));
+		Assert.assertThat(Fibonacci.fib(1), Matchers.equalTo(1L));
+		Assert.assertThat(Fibonacci.fib(2), Matchers.equalTo(1L));
+		Assert.assertThat(Fibonacci.fib(3), Matchers.equalTo(2L));
+		Assert.assertThat(Fibonacci.fib(4), Matchers.equalTo(3L));
+		Assert.assertThat(Fibonacci.fib(5), Matchers.equalTo(5L));
+	}
+
+}
