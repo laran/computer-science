87bfa6fa149802e2ecbdbd61b05aa05266c98cf4
Laran Evans <laran@laranevans.com>
2018-10-03 22:19:42 -0700

    Fixed PermuteTest

--- java/src/test/java/com/laranevans/cs/structures/lists/algorithms/PermuteTest.java
+++ java/src/test/java/com/laranevans/cs/structures/lists/algorithms/PermuteTest.java
@@ -1,7 +1,7 @@
 /* (C) Copyright 2017-2018 Laran Evans */
 package com.laranevans.cs.structures.lists.algorithms;
 
-import org.hamcrest.Matchers;
+import static org.hamcrest.Matchers.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,9 +14,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class PermuteTest {
 
 	@Test
-	@DisplayName("should permute a String properly")
 	public void shouldPermuteAString() {
-		assertThat(new Permute().permute("abc"), Matchers.equalTo(
+		assertThat(new Permute().permute("abc"), equalTo(
 			Arrays.asList("abc", "acb", "bac", "bca", "cba", "cab")
 		));
 	}
@@ -24,16 +23,11 @@ public class PermuteTest {
 	@Test
 	public void shouldPermuteAnArrayOfObjects() {
 		List<Object[]> output = new Permute().permute(new Object[]{'a', 'b', 'c'});
-		assertThat(
-			output,
-			Matchers.equalTo(
-				Arrays.asList(
-					new Object[]{'a', 'b', 'c'},
-					new Object[]{'a', 'c', 'b'},
-					new Object[]{'b', 'a', 'c'},
-					new Object[]{'b', 'c', 'a'},
-					new Object[]{'c', 'b', 'a'},
-					new Object[]{'c', 'a', 'b'})
-			));
+		assertThat(output.get(0), equalTo(new Object[]{'a', 'b', 'c'}));
+		assertThat(output.get(1), equalTo(new Object[]{'a', 'c', 'b'}));
+		assertThat(output.get(2), equalTo(new Object[]{'b', 'a', 'c'}));
+		assertThat(output.get(3), equalTo(new Object[]{'b', 'c', 'a'}));
+		assertThat(output.get(4), equalTo(new Object[]{'c', 'b', 'a'}));
+		assertThat(output.get(5), equalTo(new Object[]{'c', 'a', 'b'}));
 	}
 }
