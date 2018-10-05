dff7f236038092be2ee0e0943bdeff61c9197921
Laran Evans <laran@laranevans.com>
2018-10-05 15:48:04 -0700

    Updated graph DFS to include the iterative approach.

--- java/src/main/java/com/laranevans/cs/structures/graphs/algorithms/DepthFirst.java
+++ java/src/main/java/com/laranevans/cs/structures/graphs/algorithms/DepthFirst.java
@@ -6,32 +6,51 @@ import com.laranevans.cs.structures.graphs.GraphNode;
 
 import java.util.Objects;
 import java.util.Set;
+import java.util.Stack;
 import java.util.TreeSet;
 
 public class DepthFirst {
 
-	public static GraphNode search(Graph graph, GraphNode root, GraphNodeMatcher matcher) {
-		return search(graph, root, matcher, new TreeSet<>());
+	/**
+	 *
+	 * @param graph
+	 * @param root
+	 * @param matcher
+	 * @return
+	 */
+	public static GraphNode searchRecursively(Graph graph, GraphNode root, GraphNodeMatcher matcher) {
+		return searchRecursively(graph, root, matcher, new TreeSet<>());
 	}
 
-	private static GraphNode search(Graph graph, GraphNode root, GraphNodeMatcher matcher, Set<String> visitedNodes) {
+	/**
+	 *
+	 * @param graph
+	 * @param root
+	 * @param matcher
+	 * @return
+	 */
+	public static GraphNode searchIteratively(Graph graph, GraphNode root, GraphNodeMatcher matcher) {
+		return searchIteratively(graph, root, matcher, new TreeSet<>());
+	}
 
-		// Ensure we visit each node exactly once.
+	private static GraphNode searchRecursively(Graph graph, GraphNode root, GraphNodeMatcher matcher, Set<String> visitedNodes) {
+
+		// Ensure we visit each node at most once.
 		if (visitedNodes.contains(root.getId())) {
 			return null;
 		}
 
-		// Does it match this node?
+		// Remember that we visited this node
+		visitedNodes.add(root.getId());
+
+		// Does matcher match this node?
 		if (matcher.matches(root)) {
 			return root;
 		}
 
-		// If not, remember that we visited this node ...
-		visitedNodes.add(root.getId());
-
-		// ... and recurse to check all connected GraphNodes
+		// If not, recurse to check all connected GraphNodes
 		for (String connectedNodeId : root.getEdges().keySet()) {
-			GraphNode match = search(graph, graph.getNode(connectedNodeId), matcher, visitedNodes);
+			GraphNode match = searchRecursively(graph, graph.getNode(connectedNodeId), matcher, visitedNodes);
 			if (!Objects.isNull(match)) {
 				return match;
 			}
@@ -41,4 +60,37 @@ public class DepthFirst {
 		return null;
 	}
 
+	private static GraphNode searchIteratively(Graph graph, GraphNode root, GraphNodeMatcher matcher, Set<String> visitedNodes) {
+
+		// Using a Stack makes it depth-first.
+		// Replacing the Stack with a Queue would make it breadth-first.
+		Stack<String> stack = new Stack<>();
+		stack.push(root.getId());
+
+		while(!stack.isEmpty()) {
+			String nodeId = stack.pop();
+
+			// Ensure we visit each node at most once.
+			if(visitedNodes.contains(nodeId)) {
+				continue;
+			}
+
+			// Remember that we visited this node
+			visitedNodes.add(nodeId);
+
+			// Does matcher match this node?
+			GraphNode node = graph.getNode(nodeId);
+			if (matcher.matches(node)) {
+				return node;
+			}
+
+			// If not, push all neighbors onto the stack
+			for(String neighborId:node.getEdges().keySet()) {
+				stack.push(neighborId);
+			}
+		}
+
+		// Oh well, we tried :/
+		return null;
+	}
 }
