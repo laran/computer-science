52e00406bc89352fa21cb7c7d886273018aecc0b
Laran Evans <laran@laranevans.com>
2018-10-08 22:38:55 -0400

    Symbol table for letters.

---
+++ java/src/test/java/com/laranevans/cs/Letters.java
@@ -0,0 +1,11 @@
+/* (C) Copyright 2017-2018 Laran Evans */
+package com.laranevans.cs;
+
+/**
+ * Symbol table for letters.
+ */
+public class Letters {
+	public static String A = "A", B = "B", C = "C", D = "D", E = "E", F = "F", G = "G", H = "H", I = "I",
+		J = "J", K = "K", L = "L", M = "M", N = "N", O = "O", P = "P", Q = "Q", R = "R", S = "S", T = "T",
+		U = "U", V = "V", W = "W", X = "X", Y = "Y", Z = "Z";
+}
